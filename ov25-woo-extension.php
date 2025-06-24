0618b5262a2a39f52398ea0fdcb90a312281514d
tom williams <tom.williams@orbital.vision>
2025-06-24 11:57:05 +0100

    ov25-ui v0.3.85

--- ov25-woo-extension.php
+++ ov25-woo-extension.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: OV25
  * Description: Show off your product catalogue in 3D, with the worlds most advanced product configurator. Inifinite variations, infinite possibilities.
- * Version: .0.1.67
+ * Version: .0.1.68
  * Author: Orbital Vision
  * Author URI: https://ov25.orbitalvision.com
  * Text Domain: ov25-woo-extension
@@ -164,7 +164,7 @@ if ( ! class_exists( 'ov25_woo_extension' ) ) :
 		 *
 		 * @var string
 		 */
-		public $version = '.0.1.67';
+		public $version = '.0.1.68';
 
 		/**
 		 * Constructor.
