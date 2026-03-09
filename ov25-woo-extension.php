d938bb741fc8340a8f3dde45d950387ebac15739
Ziggy <ziggy@orbital.vision>
2026-03-09 09:35:50 +0000

    Release 0.3.43

--- ov25-woo-extension.php
+++ ov25-woo-extension.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: OV25
  * Description: Show off your product catalogue in 3D, with the worlds most advanced product configurator. Inifinite variations, infinite possibilities.
- * Version: 0.3.46
+ * Version: 0.3.43
  * Author: Orbital Vision
  * Author URI: https://ov25.orbital.vision
  * Text Domain: ov25-woo-extension
@@ -202,7 +202,7 @@ if ( ! class_exists( 'ov25_woo_extension' ) ) :
 		 *
 		 * @var string
 		 */
-		public $version = '0.3.46';
+		public $version = '0.3.43';
 
 		/**
 		 * Constructor.
